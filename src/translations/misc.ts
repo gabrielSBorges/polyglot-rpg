a3174c8b8a01d1fe6527d24c7373c9c052cda858
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-02-09 00:20:51 -0300

    adding misc translations

---
+++ src/translations/misc.ts
@@ -0,0 +1,10 @@
+import { locale } from "../utils/locale";
+
+export const misc = {
+    ...locale,
+    
+    internal_server_error: {
+        ptBR: 'Desculpe! Ocorreu um erro interno.',
+        en: 'Sorry! An internal error has ocurred.'
+    }
+}

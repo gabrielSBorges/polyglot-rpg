951ac30f6bc799900718938767982b8320cda49a
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-02-13 16:12:42 -0300

    adding misc to polyglot

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { users } from "./translations/users";
 import { yup } from "./translations/yup";
 import { fields } from "./translations/fields";
+import { misc } from "./translations/misc";
+import { users } from "./translations/users";
 
 export const polyglot = {
     yup,
     fields,
+    misc,
     users
 };

3c27b53515d9cd4c0bcb6a4f69c67186496107fb
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-02-14 23:25:03 -0300

    creating database module

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { yup } from "./translations/yup";
 import { fields } from "./translations/fields";
 import { misc } from "./translations/misc";
+import { database } from './translations/database';
 import { users } from "./translations/users";
 
 export const polyglot = {
     yup,
     fields,
     misc,
+    database,
     users
 };
---
+++ src/translations/database.ts
@@ -0,0 +1,10 @@
+import { locale } from "../utils/locale";
+
+export const database = {
+    ...locale,
+    
+    duplicate_key: {
+        ptBR: '{{key}} já está em uso.',
+        en: '{{key}} is already in use.'
+    }
+}
